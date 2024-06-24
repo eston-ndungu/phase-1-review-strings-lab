fb2f105a16f161041b8eade4ad7815208876c7d0
ESTON NDUNGU <estonndungu1@gmail.com>
2024-06-24 16:25:57 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code in this file!
+
+//Defines current user
+const currentUser = "Eston Ndungu"
+
+
+// Joins strings using Interpolation
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+
+// Changes the String to uppercase 
+const excitedWelcomeMessage= welcomeMessage.toUpperCase()
+
+// Short greeting contains the message 'welcome' and the first initial of the current user 'E'
+const shortGreeting = `Welcome, ${currentUser.charAt(0)}!`
+
